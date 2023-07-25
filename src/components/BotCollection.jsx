c8061a9e3d4a8fd318d3ffb777ac452e185d6301
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-26 01:41:55 +0300

    Changed background color and adjusted responsiveness.

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -5,10 +5,13 @@ const BotCollection = ({ bots }) => {
     <div className="container-fluid">
       <div className="row">
         {bots.map((bot) => (
-          <div key={bot.id} className="col-sm-6 col-md-4 col-lg-3 mb-2">
+          <div
+            key={bot.id}
+            className="col-sm-6 col-md-4 col-lg-3 col-xxl-2 mb-2"
+          >
             <div className="card">
               <img
-                className="card-img-top"
+                className="card-img-top bg-body-secondary"
                 src={bot.avatar_url}
                 alt={bot.name}
               />
