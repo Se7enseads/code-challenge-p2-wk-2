9cf01506dc925eb1f36e2b91dcd16685fd67862a
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-26 00:56:02 +0300

    Update Bot collection styling.

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -5,15 +5,15 @@ const BotCollection = ({ bots }) => {
     <div className="container-fluid">
       <div className="row">
         {bots.map((bot) => (
-          <div key={bot.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
-            <div className="card h-100">
+          <div key={bot.id} className="col-sm-6 col-md-4 col-lg-3 mb-2">
+            <div className="card">
               <img
                 className="card-img-top"
                 src={bot.avatar_url}
                 alt={bot.name}
               />
               <div className="card-body">
-                <h4 className="card-title">Name: {bot.name}</h4>
+                <h5 className="card-title">Name: {bot.name}</h5>
                 <p className="card-text">Bot Class: {bot.bot_class}</p>
                 <p className="card-text">{bot.catchphrase}</p>
               </div>
