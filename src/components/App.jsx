ad0165bcf858fb5d633fc26d3ba484914d03c5f3
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-28 18:38:42 +0300

    Changed the Database URL to npoint url.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
 
-const API_URL = "http://localhost:3000/bots";
+const API_URL = "https://api.npoint.io/d2caaa22d430486d2ff6/bots";
 
 const App = () => {
   const [bots, setBots] = useState([]);
