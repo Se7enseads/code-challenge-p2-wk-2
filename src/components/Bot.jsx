edb8a37239cb31a46cc24ba2d71d31cff6352811
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-30 17:14:27 +0300

    Added conditional rendering on the discharge bot / X button.

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -2,6 +2,10 @@ const Bot = ({ bot, click, location }) => {
   const { avatar_url, name, bot_class, catchphrase, health, damage, armor } =
     bot;
 
+  const handleClick = () => {
+    click(bot);
+  };
+
   let sizes;
 
   location === "collection"
@@ -12,22 +16,35 @@ const Bot = ({ bot, click, location }) => {
     <div
       className={`col-sm-${sizes.sm} col-md-${sizes.md} col-lg-${sizes.lg} col-xxl-${sizes.xxl} mb-2`}
     >
-      <div className="card" onClick={() => click(bot)}>
+      <div className="card">
         <img
           className="card-img-top bg-body-secondary"
           src={avatar_url}
           alt={name}
+          onClick={handleClick}
         />
-        <div className="card-body">
+        <div className="card-body" onClick={handleClick}>
           <h5 className="card-title">Name: {name}</h5>
           <p className="card-text">Bot Class: {bot_class}</p>
           <p className="card-text">{catchphrase}</p>
         </div>
-        <ul className="list-group list-group-flush">
+        <ul className="list-group list-group-flush" onClick={handleClick}>
           <li className="list-group-item">Health: {health}</li>
           <li className="list-group-item">Damage: {damage}</li>
           <li className="list-group-item">Armor: {armor}</li>
         </ul>
+        {location === "army" ? (
+          <div className="card-footer">
+            <div className="row mx-auto">
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => console.log("clicked!")}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
