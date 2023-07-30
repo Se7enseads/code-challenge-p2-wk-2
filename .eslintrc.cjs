7a5ad3a65a587204605c0540d327cc15640974d5
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-31 00:37:31 +0300

    Integrated ESlint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  settings: {
+    react: {
+      version: "detect",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": [0],
+  },
+};
